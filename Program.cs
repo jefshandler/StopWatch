8b6cd45934c3d4dd573fcec8adff9254a137de92
Jeferson Shandler Stecklein <jef_shandler@msn.com>
2023-08-20 20:06:58 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopWatch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void PreStart(int time)
+    {
+        Console.Clear();
+        Console.WriteLine("Ready...");
+        Thread.Sleep(500);
+        Console.WriteLine("Set...");
+        Thread.Sleep(500);
+        Console.WriteLine("Go...");
+        Thread.Sleep(1000);
+        Start(time);
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundo => 10s =10 segundos");
+        Console.WriteLine("M = Minuto => 1m = 1 minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+        int multiplier = 1;
+        if (type == 'm')
+            multiplier = 60;
+        if (type == 0)
+            System.Environment.Exit(0);
+        PreStart(time * multiplier);
+
+        //Console.WriteLine(type);
+        //Console.WriteLine(time);
+
+    }
+    static void Start(int time)
+    {
+        int currentTime = 0;
+
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+        Console.Clear();
+        Console.WriteLine("StopWatch finalizado.");
+        Thread.Sleep(1000);
+        Menu();
+    }
+    
+}
